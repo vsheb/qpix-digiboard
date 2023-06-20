8f5cc824475ac077a4fa2e9eb4e31b0b3eb8d6d6
Vasily Shebalin <shebalin@hawaii.edu>
2023-06-20 08:24:59 +0700

    the board is fully routed for the first time

--- footprint/gen_qpix_footprint.py
+++ footprint/gen_qpix_footprint.py
@@ -14,18 +14,18 @@ kicad_mod.append(Text(type='reference', text='REF**', at=[0, -3], layer='F.SilkS
 kicad_mod.append(Text(type='value', text=footprint_name, at=[1.5, 3], layer='F.Fab'))
 
 
-scale = 100.0
+scale = 4.0
 xmax, xmin = 0, 0
 ymax, ymin = 0, 0
 cnt = 0
-w, l = 0.3, 1.0
+w, l = 0.2, 0.2
 
 
 with open('./qpix_pinout.csv', mode ='r') as f :
   reader = csv.DictReader(f)
   for r in reader : 
-    x    = int(r["X"].strip())/scale
-    y    = int(r["Y"].strip())/scale
+    x    = float(r["X"].strip())/1000.0
+    y    = float(r["Y"].strip())/1000.0
     if cnt == 0 : 
       xmin, ymin= x,y
     else : 
@@ -35,6 +35,14 @@ with open('./qpix_pinout.csv', mode ='r') as f :
       if y > ymax : ymax = y
     cnt += 1
 
+x_die_max = xmax
+y_die_max = ymax
+
+xmax = xmax*scale
+ymax = ymax*scale
+
+y = float(r["Y"].strip())*scale/1000.0
+# centering
 x0, y0 = -(xmax - xmin)/2.0, (ymax - ymin)/2.0
 
 
@@ -48,8 +56,8 @@ with open('./qpix_pinout.csv', mode ='r') as f :
      
     num  = int(r["Pin"])
     name = r["Name"].strip()
-    x    = x0 + int(r["X"].strip())/scale
-    y    = y0 - int(r["Y"].strip())/scale
+    x    = x0 + float(r["X"].strip())*scale/1000.0
+    y    = y0 - float(r["Y"].strip())*scale/1000.0
     side = r["Side"].strip()
 
     if side == "top" or side == "bottom":
@@ -61,6 +69,7 @@ with open('./qpix_pinout.csv', mode ='r') as f :
 
   
 
+    angle = 0
     kicad_mod.append(Pad(number=num, type=Pad.TYPE_SMT, shape=Pad.SHAPE_RECT,
                          rotation = angle,
                          at=[x, y], size=sz, drill=0, layers=Pad.LAYERS_SMT))
@@ -68,10 +77,12 @@ with open('./qpix_pinout.csv', mode ='r') as f :
 x_ss, y_ss = 1, 1
 # create silscreen
 kicad_mod.append(RectLine(start=[x0+xmin-x_ss, y0-ymin+y_ss], end=[x0+xmax+x_ss, y0-ymax-y_ss], layer='F.SilkS'))
+kicad_mod.append(RectLine(start=[-x_die_max/2, -y_die_max/2], end=[x_die_max/2, y_die_max/2], layer='F.SilkS'))
 
 x_cy, y_cy = 1.5, 1.5
 # create courtyard
 kicad_mod.append(RectLine(start=[x0+xmin-x_cy, y0-ymin+y_cy], end=[x0+xmax+x_cy, y0-ymax-y_cy], layer='F.CrtYd'))
+
 
 # add model
 kicad_mod.append(Model(filename="example.3dshapes/example_footprint.wrl",
